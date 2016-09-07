dbff2939cb0be25372cefea26770d64f407c9ba2
dpennebacker <dpennebacker@pvsd.org>
2016-09-07 14:20:47 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("What is going on?");
+        System.out.print("Hello World");
+        //The above is the two ways we need to
+        // know how to output text
+        //Two types of comments
+        // 1. Line comment and that is done with a //
+        // 2. Block comment. That is done with a /*
+        System.out.print(1);
+        System.out.print(1.0);
+
+    }
+}
